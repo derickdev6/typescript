c909acb214e9ad26b83fa4306e09ab64a034ffaa
Derick Robinson <derickdev6@gmail.com>
2020-12-23 11:45:23 -0500

    Cap 10 - Any

---
+++ dist/typeany.js
@@ -0,0 +1,16 @@
+"use strict";
+//Explict any
+let id;
+id = 1;
+id = 'A';
+id = 0x214ff;
+id = 0b010110;
+id = 0o141124;
+console.log(`ID: ${id}`);
+let otherid = 1;
+otherid = 0xffffff;
+console.log(`ID: ${otherid}`);
+let surprise = `hi typescript`;
+let resstring = surprise.substring(3);
+console.log(resstring);
+console.log(`Use any only as last resource or for type changing variables`);
