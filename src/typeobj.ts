a5cf8aec67a1adec4099ae79947cad8ac487baf9
Derick Robinson <derickdev6@gmail.com>
2020-12-23 16:13:35 -0500

    Cap 13 - Object class vs object type

---
+++ src/typeobj.ts
@@ -0,0 +1,23 @@
+// Object
+
+let user:object;
+user = {};
+
+user = {
+    id: 1,
+    name: `Derick`,
+    isPro: true,
+    getname : function(){return `Derick`}
+};
+
+console.log(user);
+
+//Object vs object (JS class vs TS type)
+
+const myObject = {
+    id: 1,
+    name: `Derick`,
+    isPro: true
+};
+console.log(myObject instanceof Object);
+console.log(myObject.name);
