184dde9c8e237d25806f5560a4fd12119e616920
Derick Robinson <derickdev6@gmail.com>
2021-02-18 15:41:52 -0500

    Cap 26 - Getters and Setters

--- src/clases.js
+++ src/clases.js
@@ -11,7 +11,7 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     }
     return privateMap.get(receiver);
 };
-var _id, _title, _pictures, _description;
+var _id, _title, _orientation, _id_1, _pictures, _title_1, _description;
 var PhotoOrientation;
 (function (PhotoOrientation) {
     PhotoOrientation[PhotoOrientation["Landscape"] = 0] = "Landscape";
@@ -23,35 +23,93 @@ var PhotoOrientation;
 var Picture = /** @class */ (function () {
     //Constructor
     function Picture(id, title, orientation) {
-        this.title = 'Default';
-        this.orientation = PhotoOrientation.Default;
-        this.id = id;
-        this.title = title;
-        this.orientation = orientation;
+        //Properties
+        _id.set(this, void 0);
+        _title.set(this, 'Default');
+        _orientation.set(this, PhotoOrientation.Default);
+        __classPrivateFieldSet(this, _id, id);
+        if (title)
+            __classPrivateFieldSet(this, _title, title);
+        if (orientation)
+            __classPrivateFieldSet(this, _orientation, orientation);
     }
+    Object.defineProperty(Picture.prototype, "Id", {
+        //Getters
+        get: function () { return __classPrivateFieldGet(this, _id); },
+        enumerable: false,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(Picture.prototype, "Title", {
+        get: function () { return __classPrivateFieldGet(this, _title); },
+        //Setters
+        set: function (value) {
+            __classPrivateFieldSet(this, _title, value);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(Picture.prototype, "Orientation", {
+        get: function () { return __classPrivateFieldGet(this, _orientation); },
+        set: function (value) {
+            __classPrivateFieldSet(this, _orientation, value);
+        },
+        enumerable: false,
+        configurable: true
+    });
     //Methods
     Picture.prototype.toString = function () {
-        return "Picture - Title:" + this.title + " - Orientation:" + this.orientation + " - id:" + this.id;
+        return "Picture - Title:" + __classPrivateFieldGet(this, _title) + " - Orientation:" + __classPrivateFieldGet(this, _orientation) + " - id:" + __classPrivateFieldGet(this, _id);
     };
     return Picture;
 }());
+_id = new WeakMap(), _title = new WeakMap(), _orientation = new WeakMap();
 var Album = /** @class */ (function () {
-    function Album(id, title, pictures, description) {
-        _id.set(this, void 0);
-        _title.set(this, 'Default');
+    function Album(id, pictures, title, description) {
+        _id_1.set(this, void 0);
         _pictures.set(this, []);
+        _title_1.set(this, 'Default');
         _description.set(this, 'Default');
-        __classPrivateFieldSet(this, _id, id);
-        __classPrivateFieldSet(this, _title, title);
-        __classPrivateFieldSet(this, _description, description);
+        __classPrivateFieldSet(this, _id_1, id);
         __classPrivateFieldSet(this, _pictures, pictures);
+        if (title)
+            __classPrivateFieldSet(this, _title_1, title);
+        if (description)
+            __classPrivateFieldSet(this, _description, description);
     }
+    Object.defineProperty(Album.prototype, "Id", {
+        //Getters
+        get: function () { return __classPrivateFieldGet(this, _id_1); },
+        //Setters
+        set: function (value) { __classPrivateFieldSet(this, _id_1, value); },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Album.prototype, "Title", {
+        get: function () { return __classPrivateFieldGet(this, _title_1); },
+        set: function (value) { __classPrivateFieldSet(this, _title_1, value); },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Album.prototype, "Description", {
+        get: function () { return __classPrivateFieldGet(this, _description); },
+        set: function (value) { __classPrivateFieldSet(this, _description, value); },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Album.prototype, "Pictures", {
+        get: function () { return __classPrivateFieldGet(this, _pictures); },
+        set: function (value) { __classPrivateFieldSet(this, _pictures, value); },
+        enumerable: false,
+        configurable: true
+    });
     Album.prototype.toString = function () {
-        return "Album - Title:" + __classPrivateFieldGet(this, _title) + " - Description:" + __classPrivateFieldGet(this, _description) + " - id:" + __classPrivateFieldGet(this, _id);
+        return "Album - Title:" + __classPrivateFieldGet(this, _title_1) + " - Description:" + __classPrivateFieldGet(this, _description) + " - id:" + __classPrivateFieldGet(this, _id_1);
     };
     Album.prototype.showPictures = function () {
         __classPrivateFieldGet(this, _pictures).forEach(function (element) {
-            console.log(element.toString);
+            console.log(element.toString());
         });
     };
     Album.prototype.setDescription = function (newdescription) {
@@ -62,16 +120,18 @@ var Album = /** @class */ (function () {
     };
     return Album;
 }());
-_id = new WeakMap(), _title = new WeakMap(), _pictures = new WeakMap(), _description = new WeakMap();
-// Accessing public members
-var myalbum = new Album(1, 'Personal Album', []);
-myalbum.setDescription('Hey this is my new album');
-var mypic1 = new Picture(1);
-var mypic2 = new Picture(2, 'Pic2');
-var mypic3 = new Picture(3, undefined, PhotoOrientation.Panorama);
-var mypic4 = new Picture(4, 'Pic4', PhotoOrientation.Landscape);
-myalbum.addPicture(mypic1);
-myalbum.addPicture(mypic2);
-myalbum.addPicture(mypic3);
-myalbum.addPicture(mypic4);
-console.log(myalbum);
+_id_1 = new WeakMap(), _pictures = new WeakMap(), _title_1 = new WeakMap(), _description = new WeakMap();
+// Accessing members
+var myalbum = new Album(1, []);
+var pic1 = new Picture(1);
+var pic2 = new Picture(1, 'Picture number 2');
+var pic3 = new Picture(1, undefined, PhotoOrientation.Panorama);
+var pic4 = new Picture(1, 'Picture number 4', PhotoOrientation.Portrait);
+myalbum.addPicture(pic1);
+myalbum.addPicture(pic2);
+myalbum.addPicture(pic3);
+myalbum.addPicture(pic4);
+myalbum.Description = 'Hey this is my album';
+myalbum.Title = 'First almbum';
+console.log(myalbum.toString());
+myalbum.showPictures();
--- src/clases.ts
+++ src/clases.ts
@@ -8,68 +8,92 @@ enum PhotoOrientation {
 
 class Picture {
     //Properties
-    private id: number;
-    private title?: string = 'Default';
-    private orientation?: PhotoOrientation = PhotoOrientation.Default;
+    #id: number;
+    #title: string = 'Default';
+    #orientation: PhotoOrientation = PhotoOrientation.Default;
 
     //Constructor
     constructor(id: number, title?: string, orientation?: PhotoOrientation) {
 
-        this.id = id;
-        this.title = title;
-        this.orientation = orientation;
+        this.#id = id;
+        if (title) this.#title = title;
+        if (orientation) this.#orientation = orientation;
 
+    }
+    //Getters
+    get Id() { return this.#id };
+    get Title() { return this.#title };
+    get Orientation() { return this.#orientation }
+    //Setters
+    set Title(value: string) {
+        this.#title = value
+    }
+    set Orientation(value: PhotoOrientation) {
+        this.#orientation = value
     }
 
     //Methods
     toString() {
-        return `Picture - Title:${this.title} - Orientation:${this.orientation} - id:${this.id}`
+        return `Picture - Title:${this.#title} - Orientation:${this.#orientation} - id:${this.#id}`
     }
+
 }
 
 class Album {
     #id: number;
-    #title?: string = 'Default';
     #pictures: Picture[] = [];
-    #description?: string = 'Default';
+    #title: string = 'Default';
+    #description: string = 'Default';
 
-    constructor(id: number, title: string, pictures: Picture[], description?: string) {
+    constructor(id: number, pictures: Picture[], title?: string, description?: string) {
 
         this.#id = id;
-        this.#title = title;
-        this.#description = description;
         this.#pictures = pictures;
+        if (title) this.#title = title;
+        if (description) this.#description = description;
 
     }
+    //Getters
+    get Id() { return this.#id; }
+    get Title() { return this.#title }
+    get Description() { return this.#description }
+    get Pictures() { return this.#pictures }
+
+    //Setters
+    set Id(value: number) { this.#id = value }
+    set Title(value: string) { this.#title = value }
+    set Description(value: string) { this.#description = value }
+    set Pictures(value: Picture[]) { this.#pictures = value }
 
     toString() {
         return `Album - Title:${this.#title} - Description:${this.#description} - id:${this.#id}`
     }
     showPictures() {
         this.#pictures.forEach(element => {
-            console.log(element.toString);
+            console.log(element.toString());
+            
         });
     }
-    setDescription(newdescription:string){
+    setDescription(newdescription: string) {
         this.#description = newdescription;
     }
-    addPicture(newpic:Picture){
+    addPicture(newpic: Picture) {
         this.#pictures.push(newpic);
     }
 }
 
-// Accessing public members
+// Accessing members
 
-const myalbum = new Album(1, 'Personal Album', []);
-myalbum.setDescription('Hey this is my new album');
-
-const mypic1 = new Picture(1);
-const mypic2 = new Picture(2, 'Pic2');
-const mypic3 = new Picture(3, undefined, PhotoOrientation.Panorama);
-const mypic4 = new Picture(4, 'Pic4', PhotoOrientation.Landscape);
-myalbum.addPicture(mypic1);
-myalbum.addPicture(mypic2);
-myalbum.addPicture(mypic3);
-myalbum.addPicture(mypic4);
-
-console.log(myalbum);
+const myalbum = new Album(1, []);
+const pic1 = new Picture(1);
+const pic2 = new Picture(1, 'Picture number 2');
+const pic3 = new Picture(1, undefined, PhotoOrientation.Panorama);
+const pic4 = new Picture(1, 'Picture number 4', PhotoOrientation.Portrait);
+myalbum.addPicture(pic1);
+myalbum.addPicture(pic2);
+myalbum.addPicture(pic3);
+myalbum.addPicture(pic4);
+myalbum.Description = 'Hey this is my album';
+myalbum.Title = 'First almbum';
+console.log(myalbum.toString());
+myalbum.showPictures();
