8b7550ee776cf3348a659aa3eeaeb38e52033de3
Derick Robinson <derickdev6@gmail.com>
2021-02-18 15:14:08 -0500

    Cap 25 - Private and # classes

--- src/clases.js
+++ src/clases.js
@@ -1,3 +1,17 @@
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _id, _title, _pictures, _description;
 var PhotoOrientation;
 (function (PhotoOrientation) {
     PhotoOrientation[PhotoOrientation["Landscape"] = 0] = "Landscape";
@@ -23,21 +37,41 @@ var Picture = /** @class */ (function () {
 }());
 var Album = /** @class */ (function () {
     function Album(id, title, pictures, description) {
-        this.title = 'Default';
-        this.pictures = [];
-        this.description = 'Default';
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.pictures = pictures;
+        _id.set(this, void 0);
+        _title.set(this, 'Default');
+        _pictures.set(this, []);
+        _description.set(this, 'Default');
+        __classPrivateFieldSet(this, _id, id);
+        __classPrivateFieldSet(this, _title, title);
+        __classPrivateFieldSet(this, _description, description);
+        __classPrivateFieldSet(this, _pictures, pictures);
     }
     Album.prototype.toString = function () {
-        return "Album - Title:" + this.title + " - Description:" + this.description + " - id:" + this.id;
+        return "Album - Title:" + __classPrivateFieldGet(this, _title) + " - Description:" + __classPrivateFieldGet(this, _description) + " - id:" + __classPrivateFieldGet(this, _id);
     };
     Album.prototype.showPictures = function () {
-        this.pictures.forEach(function (element) {
+        __classPrivateFieldGet(this, _pictures).forEach(function (element) {
             console.log(element.toString);
         });
     };
+    Album.prototype.setDescription = function (newdescription) {
+        __classPrivateFieldSet(this, _description, newdescription);
+    };
+    Album.prototype.addPicture = function (newpic) {
+        __classPrivateFieldGet(this, _pictures).push(newpic);
+    };
     return Album;
 }());
+_id = new WeakMap(), _title = new WeakMap(), _pictures = new WeakMap(), _description = new WeakMap();
+// Accessing public members
+var myalbum = new Album(1, 'Personal Album', []);
+myalbum.setDescription('Hey this is my new album');
+var mypic1 = new Picture(1);
+var mypic2 = new Picture(2, 'Pic2');
+var mypic3 = new Picture(3, undefined, PhotoOrientation.Panorama);
+var mypic4 = new Picture(4, 'Pic4', PhotoOrientation.Landscape);
+myalbum.addPicture(mypic1);
+myalbum.addPicture(mypic2);
+myalbum.addPicture(mypic3);
+myalbum.addPicture(mypic4);
+console.log(myalbum);
--- src/clases.ts
+++ src/clases.ts
@@ -8,12 +8,12 @@ enum PhotoOrientation {
 
 class Picture {
     //Properties
-    id: number;
-    title?: string = 'Default';
-    orientation?: PhotoOrientation = PhotoOrientation.Default;
+    private id: number;
+    private title?: string = 'Default';
+    private orientation?: PhotoOrientation = PhotoOrientation.Default;
 
     //Constructor
-    constructor(id: number, title: string, orientation: PhotoOrientation) {
+    constructor(id: number, title?: string, orientation?: PhotoOrientation) {
 
         this.id = id;
         this.title = title;
@@ -28,26 +28,48 @@ class Picture {
 }
 
 class Album {
-    id: number;
-    title?: string = 'Default';
-    pictures: Picture[] = [];
-    description?: string = 'Default';
+    #id: number;
+    #title?: string = 'Default';
+    #pictures: Picture[] = [];
+    #description?: string = 'Default';
 
-    constructor(id: number, title: string, pictures: Picture[], description: string) {
+    constructor(id: number, title: string, pictures: Picture[], description?: string) {
 
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.pictures = pictures;
+        this.#id = id;
+        this.#title = title;
+        this.#description = description;
+        this.#pictures = pictures;
 
     }
 
     toString() {
-        return `Album - Title:${this.title} - Description:${this.description} - id:${this.id}`
+        return `Album - Title:${this.#title} - Description:${this.#description} - id:${this.#id}`
     }
     showPictures() {
-        this.pictures.forEach(element => {
+        this.#pictures.forEach(element => {
             console.log(element.toString);
         });
     }
+    setDescription(newdescription:string){
+        this.#description = newdescription;
+    }
+    addPicture(newpic:Picture){
+        this.#pictures.push(newpic);
+    }
 }
+
+// Accessing public members
+
+const myalbum = new Album(1, 'Personal Album', []);
+myalbum.setDescription('Hey this is my new album');
+
+const mypic1 = new Picture(1);
+const mypic2 = new Picture(2, 'Pic2');
+const mypic3 = new Picture(3, undefined, PhotoOrientation.Panorama);
+const mypic4 = new Picture(4, 'Pic4', PhotoOrientation.Landscape);
+myalbum.addPicture(mypic1);
+myalbum.addPicture(mypic2);
+myalbum.addPicture(mypic3);
+myalbum.addPicture(mypic4);
+
+console.log(myalbum);
